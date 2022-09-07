db7686b190481e38d81c37f9a3fb6335795e840d
Vitaliy <vladimir42142@mail.ru>
2022-09-07 16:00:47 +0300

    Task015

---
+++ Seminar2/Task013/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if (number > 99)
+
+// Вариант со строкой. 
+// Console.Write("Введи число: ");
+// string number = Console.ReadLine();
+
+// if (number.Length > 2)
+// {
+//     Console.Write(number[2]);
+// }
+// else
+// {
+//     Console.WriteLine("Третьей цифры нет");
+// }
+
+// Вариант с делением на 10 и берем остаток от деления на 10
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = 0;                                     // вводим переменную result
+if (number > 99)                                    // если число меньше трехзначного
+{
+    while (number > 100)
+    {   
+        result = number % 10;                       // берем остаток от деления на 10
+        number = number / 10;                       // делим число на 10
+    }
+    Console.Write(result);                          // печатаем результат
+}
+
+else
+{ 
+        Console.WriteLine("Третьей цифры нет");
+}
+    
+    
+
+         
+    
---
+++ Seminar2/Task015/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Write("Введите число от 1 до 7: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine()); 
+
+if (dayNumber >= 1 && dayNumber <= 7) //[1;7]. Проверка числа 
+{
+    if (dayNumber == 6)
+    {
+        Console.WriteLine("Да");
+    }
+    else if (dayNumber == 7) 
+    {
+        Console.WriteLine("Да"); 
+    }
+    else
+    {
+        Console.WriteLine("Нет"); 
+    }
+}
+else
+{
+    Console.WriteLine("Error 404, day not found");
+}
+
